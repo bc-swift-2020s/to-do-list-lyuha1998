82fd059a03ab826ea0f2ed62056c245998319304
Huiyi Victoria Lyu <lyuha@bc.edu>
2020-02-10 15:27:29 -0500

    done

--- todolist/TODODETAILSTableViewController.swift
+++ todolist/TODODETAILSTableViewController.swift
@@ -48,21 +48,6 @@ class TODODETAILSTableViewController: UITableViewController {
         toDoItem = ToDoItem(name:NameField.text!, date: datepicker.date, notes: noteView.text)
     }
 
-    // MARK: - Table view data source
-
-
-    
-    
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
    
     @IBAction func cancelbottonpressed(_ sender: UIBarButtonItem) {
         
